17910f544d45b0b1b91c5ffb286433884d2c18a5
Admabaga <admabaga@outlook.com>
2025-02-22 15:47:20 -0500

    Primer Commit

---
+++ js/introduccion.js
@@ -0,0 +1,10 @@
+// par comentario de una linea
+/*
+    comentario de varias lineas
+*/
+
+//console.log --> para imprimir mensajes en la consola
+
+//Concatenar --> unir dos mensajes o variables
+
+console.log('Hola, ' + 'Mundo!');
